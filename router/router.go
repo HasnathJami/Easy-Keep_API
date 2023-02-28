d68106b72eabba6c9ccb98ff676dbdd1a596fc0a
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2023-02-28 07:08:38 +0600

    Refactoring

--- router/router.go
+++ router/router.go
@@ -13,7 +13,7 @@ func Router() *mux.Router {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {w.Write([]byte("Welcome To MyWallet!"))})
 	router.HandleFunc("/login", controllers.UserLogin).Methods("POST")
 	router.HandleFunc("/register", controllers.UserRegistration).Methods("POST")
-    router.HandleFunc("/credit", controllers.CreditMoney).Methods("POST")
+        router.HandleFunc("/credit", controllers.CreditMoney).Methods("POST")
 	router.HandleFunc("/debit", controllers.DebitMoney).Methods("POST")
 	router.HandleFunc("/report", controllers.UserReport).Methods("GET")
 
